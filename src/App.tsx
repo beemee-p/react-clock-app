911d56d35b70338bb57521002e6bc6226b23f5af
Yuna Park <una@stunning.kr>
2024-01-28 00:36:35 +0900

    fix: import react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { ReactElement } from "react";
+import Clock from "./components/Clock";
 
 const App = (): ReactElement => {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Clock />
+    </div>
+  );
 };
 
 export default App;
