748baa8c1a0e5c8da8b1c26db9d402c94dd05c79
Yuna Park <una@stunning.kr>
2024-01-28 22:51:55 +0900

    add: app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ReactElement } from "react";
-import Clock from "./components/Clock";
 import { RecoilRoot } from "recoil";
+import Clock from "./components/Clock";
+import "./app.css";
 
 const App = (): ReactElement => {
   return (
     <RecoilRoot>
-      <div className="App">
+      <div className="app">
         <Clock />
       </div>
     </RecoilRoot>
