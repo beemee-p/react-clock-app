39c7315ce97fd3c3c9a34a3f61178368a2fae42a
Yuna Park <una@stunning.kr>
2024-01-28 21:30:03 +0900

    modify: cycle logic

--- src/components/Minutes.tsx
+++ src/components/Minutes.tsx
@@ -1,19 +1,28 @@
-import { ReactElement, useEffect } from "react";
-import { useRecoilState } from "recoil";
-import { minutesAtom } from "store/Time";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { ReactElement, useEffect } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { minutesAtom, secondsAtom } from 'store/Time';
 
 const Minutes = (): ReactElement => {
+  const seconds = useRecoilValue(secondsAtom);
   const [minutes, setMinutes] = useRecoilState(minutesAtom);
 
   useEffect(() => {
-    const minInterval = setInterval(
-      () => setMinutes((prevMin) => prevMin + 1),
-      60000
-    );
+    handleMinutes();
+  }, [seconds]);
 
-    return () => clearInterval(minInterval);
-  }, []);
+  function handleMinutes() {
+    setMinutes(prevMin => {
+      if (seconds === 0) {
+        if (prevMin >= 59) {
+          return 0;
+        }
+        return prevMin + 1;
+      }
 
+      return prevMin;
+    });
+  }
   return <div>Minutes : {minutes}</div>;
 };
 
