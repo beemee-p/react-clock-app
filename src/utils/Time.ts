e40a152b91b56f5882925f6239ed765f3581e62d
Yuna Park <una@stunning.kr>
2024-01-31 00:26:22 +0900

    fix: clock rotate bug

--- src/utils/Time.ts
+++ src/utils/Time.ts
@@ -1,3 +1,12 @@
+export const getCurrentTime = () => {
+  const now = new Date();
+  return {
+    hours: now.getHours(),
+    minutes: now.getMinutes(),
+    seconds: now.getSeconds(),
+  };
+};
+
 export function getHourDegree(hours: number, minutes: number) {
   return hours * 30 + 90 + (minutes / 60) * 30;
 }
