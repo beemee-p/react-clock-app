803fa7723fae709720ec59df44b8a51e95774ef7
Yuna Park <una@stunning.kr>
2024-02-01 21:55:40 +0900

    add: tooltip and remove cursor state

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,36 +1,28 @@
 import {
-  MouseEventHandler,
   ReactElement,
-  useEffect,
-  useMemo,
   useRef,
   useState,
+  useEffect,
+  MouseEventHandler,
 } from "react";
-import { useRecoilState } from "recoil";
 import { ClockType, clockRotateAtom } from "store/Rotate";
+import { useRecoilState } from "recoil";
+import { useThrottle } from "hooks/useThrottle";
 import { getCurrentTime, getHourDegree, getTimeDegree } from "utils/Time";
-import styled from "styled-components";
 import Hours from "./Hours";
 import Seconds from "./Seconds";
 import Minutes from "./Minutes";
+import Tooltip from "./Tooltip";
+import styled from "styled-components";
 import clockImg from "static/img/img-clock.webp";
-import { useThrottle } from "hooks/useThrottle";
 
 const SECOND_PER_FRAME = 16.6; // 1초당 보여져야하는 프레임을 밀리세컨으로 변환
-
-interface CursorPosition {
-  x: number;
-  y: number;
-}
 
 const Clock = (): ReactElement => {
   const now = getCurrentTime();
   const clockRef = useRef<HTMLElement>(null);
   const [, setRotate] = useRecoilState<ClockType>(clockRotateAtom);
-  const [cursorPosition, setCursorPosition] = useState<CursorPosition>({
-    x: 0,
-    y: 0,
-  });
+  const [isHover, setIsHover] = useState(false);
 
   useEffect(() => {
     const animateClock = () => {
@@ -48,38 +40,30 @@ const Clock = (): ReactElement => {
     return () => cancelAnimationFrame(animationID);
   });
 
-  const mouseCursor = useMemo(() => {
-    return (
-      <div
-        className={"cursor_tooltip"}
-        style={{
-          left: `${cursorPosition.x}px`,
-          top: `${cursorPosition.y - 30}px`,
-        }}
-      >
-        {`${now.hours} : ${now.minutes} : ${now.seconds}`}
-      </div>
-    );
-  }, [cursorPosition, now.hours, now.minutes, now.seconds]);
-
   const handleMouseMove: MouseEventHandler<HTMLDivElement> = (e) => {
     const scrollTop = clockRef.current?.scrollTop || 0;
-
-    setCursorPosition({
-      x: e.clientX,
-      y: e.clientY + scrollTop,
-    });
+    const tooltip = document.querySelector(".tooltip") as HTMLElement;
+    tooltip.style.left = `${e.clientX}px`;
+    tooltip.style.top = `${e.clientY + scrollTop - 30}px`;
   };
 
   const throttledFn = useThrottle(handleMouseMove, SECOND_PER_FRAME);
 
   return (
-    <MainClock ref={clockRef} onMouseMove={throttledFn}>
+    <MainClock
+      ref={clockRef}
+      onMouseMove={throttledFn}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
       <Hours />
       <Minutes />
       <Seconds />
       <img src={clockImg} alt={"clock"} />
-      {mouseCursor}
+
+      {isHover && (
+        <Tooltip>{`${now.hours} : ${now.minutes} : ${now.seconds}`}</Tooltip>
+      )}
     </MainClock>
   );
 };
@@ -93,35 +77,6 @@ const MainClock = styled.main`
   > img {
     width: 100%;
     height: 100%;
-  }
-  .cursor_tooltip {
-    display: none;
-  }
-
-  &:hover {
-    .cursor_tooltip {
-      display: block;
-      position: absolute;
-      width: fit-content;
-      top: 0;
-      height: auto;
-      background: linear-gradient(
-          90deg,
-          rgb(81, 119, 254) -9.37%,
-          rgba(137, 152, 255, 0) 113.12%
-        ),
-        rgb(255, 116, 181);
-      color: #ffffff;
-      border-radius: 8px;
-      padding: 6px 10px;
-      margin: 10px 0 0 10px;
-      font-size: 12px;
-      font-weight: 400;
-      line-height: 15px;
-      text-align: center;
-      letter-spacing: -0.25px;
-      white-space: nowrap;
-    }
   }
 `;
 
